b4426e3e11d7f623f2c7ca8261bc19fad0fe3cda
Arve Knudsen <arve.knudsen@gmail.com>
2010-09-25 15:27:39 +0200

    Add tests for plugin testid

---
+++ nose2/tests/_common.py
@@ -0,0 +1,23 @@
+"""Common functionality for tests."""
+class _FakeEventBase(object):
+    """Baseclass for fake Events."""
+    def __init__(self):
+        self._fake_messages = []
+
+    def message(self, msg, verbosity=(1, 2)):
+        self._fake_messages.append((msg, verbosity))
+
+class FakeStartTestEvent(_FakeEventBase):
+    """Fake StartTestEvent."""
+    def __init__(self, test):
+        super(FakeStartTestEvent, self).__init__()
+        self.test = test
+        self.result = test.defaultTestResult()
+        import time
+        self.startTime = time.time()
+
+class FakeLoadFromNameEvent(_FakeEventBase):
+    """Fake LoadFromNameEvent."""
+    def __init__(self, name):
+        super(FakeLoadFromNameEvent, self).__init__()
+        self.name = name

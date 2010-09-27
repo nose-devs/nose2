aadce4d8b76f52bbd3494473266a6a0cfeacb099
Arve Knudsen <arve.knudsen@gmail.com>
2010-09-27 21:49:46 +0200

    Move content from tests.__init__ to tests._common; single leading underscore

--- nose2/tests/_common.py
+++ nose2/tests/_common.py
@@ -3,6 +3,13 @@ import os.path
 import tempfile
 import shutil
 import sys
+import subprocess
+import unittest2
+
+HERE = os.path.dirname(__file__)
+ROOT = os.path.join(HERE, '..', '..')
+SUPPORT = os.path.join(ROOT, 'support')
+
 
 class TestCase(unittest2.TestCase):
     """TestCase extension.
@@ -16,9 +23,9 @@ class TestCase(unittest2.TestCase):
         super(TestCase, self).setUp()
         
         if self._RUN_IN_TEMP:
-            self.__orig_dir = os.getcwd()
-            work_dir = self.__work_dir = tempfile.mkdtemp()
-            os.chdir(self.__work_dir)
+            self._orig_dir = os.getcwd()
+            work_dir = self._work_dir = tempfile.mkdtemp()
+            os.chdir(self._work_dir)
             # Make sure it's possible to import modules from current directory
             sys.path.insert(0, work_dir)
 
@@ -26,8 +33,28 @@ class TestCase(unittest2.TestCase):
         super(TestCase, self).tearDown()
 
         if self._RUN_IN_TEMP:
-            os.chdir(self.__orig_dir)
-            shutil.rmtree(self.__work_dir, ignore_errors=True)
+            os.chdir(self._orig_dir)
+            shutil.rmtree(self._work_dir, ignore_errors=True)
+
+
+class FunctionalTestCase(unittest2.TestCase):
+
+    tags = ['functional']
+
+    def assertTestRunOutputMatches(self, proc, stdout=None, stderr=None):
+        cmd_stdout, cmd_stderr = proc.communicate()
+        if stdout:
+            self.assertRegexpMatches(cmd_stdout, stdout)
+        if stderr:
+            self.assertRegexpMatches(cmd_stderr, stderr)
+
+    def runIn(self, testdir, *args):
+        cmd = ['nose2'] + list(args)
+        proc = subprocess.Popen(cmd, 
+                                cwd=os.path.join(SUPPORT, testdir),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        return proc
 
 
 class _FakeEventBase(object):

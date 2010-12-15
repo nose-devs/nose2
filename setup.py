60dfd2e4d3d5c3d68b317a7f9059f17f12a18883
JP <jpellerin@gmail.com>
2010-12-15 17:06:12 -0500

    Added tox config, packaging fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='nose2',
     version='0.1',
-    entry_points= {
+    packages=find_packages(),
+    entry_points={
         'console_scripts': [
             'nose2 = nose2:main_',
             ],
-        }
+        },
+    include_package_data=True,
+    package_data={'': ['nose2/plugins/plugins.cfg']},
 )

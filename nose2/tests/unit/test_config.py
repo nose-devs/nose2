19d99f6040d1474feee0f2fb0bda7cb14fbf407c
JP <jpellerin@gmail.com>
2011-12-27 15:43:21 -0500

    Add test for as_list bugfix

--- nose2/tests/unit/test_config.py
+++ nose2/tests/unit/test_config.py
@@ -6,11 +6,25 @@ class TestConfigSession(unittest.TestCase):
     def test_can_create_session(self):
         config.Session()
 
+    def test_load_plugins_from_module_can_load_plugins(self):
+        class fakemod:
+            pass
+        f = fakemod()
+        class A(events.Plugin):
+            pass
+        f.A = A
+        session = config.Session()
+        session.loadPluginsFromModule(f)
+        assert session.plugins
+        a = session.plugins[0]
+        self.assertEqual(a.session, session)
+
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
         self.conf = config.Config([
-                ('a', ' 1 '), ('b', '  x\n  y '), ('c', '0')])
+                ('a', ' 1 '), ('b', '  x\n  y '), ('c', '0'),
+                ('d', '123')])
 
     def test_as_int(self):
         self.assertEqual(self.conf.as_int('a'), 1)
@@ -29,3 +43,5 @@ class TestConfig(unittest.TestCase):
 
     def test_as_list(self):
         self.assertEqual(self.conf.as_list('b'), ['x', 'y'])
+        self.assertEqual(self.conf.as_list('a'), ['1'])
+        self.assertEqual(self.conf.as_list('d'), ['123'])

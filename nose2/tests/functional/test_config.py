113f6f52835b9184853d8b4de966186fbdd92ef9
JP <jpellerin@gmail.com>
2011-12-27 15:46:45 -0500

    Add functional test for plugin loading

--- nose2/tests/functional/test_config.py
+++ nose2/tests/functional/test_config.py
@@ -1,6 +1,9 @@
+import sys
+
 from nose2.compat import unittest
 from nose2 import config
 from nose2.tests.functional import support_file
+
 
 class TestConfigLoading(unittest.TestCase):
 
@@ -8,6 +11,7 @@ class TestConfigLoading(unittest.TestCase):
         self.s = config.Session()
         self.s.loadConfigFiles(support_file('cfg', 'a.cfg'),
                                support_file('cfg', 'b.cfg'))
+        sys.path.insert(0, support_file('lib'))
 
     def test_session_can_load_config_files(self):
         assert self.s.config.has_section('a')
@@ -16,3 +20,9 @@ class TestConfigLoading(unittest.TestCase):
     def test_session_holds_plugin_config(self):
         plug_config = self.s.get('a')
         assert plug_config
+
+    def test_session_can_load_plugins_from_modules(self):
+        self.s.loadPlugins()
+        assert self.s.plugins
+        plug = self.s.plugins[0]
+        self.assertEqual(plug.a, 1)

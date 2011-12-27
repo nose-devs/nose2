83a2a04ec5b416e68588142ececb055d646a5449
JP <jpellerin@gmail.com>
2011-12-27 15:49:24 -0500

    Add utility function for executing test runs

--- nose2/tests/functional/__init__.py
+++ nose2/tests/functional/__init__.py
@@ -1,6 +1,23 @@
 import os
+import subprocess
 
 SUPPORT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'support'))
 
 def support_file(*path_parts):
     return os.path.join(SUPPORT, *path_parts)
+
+
+
+def run_nose2(*nose2_args, **popen_args):
+    if 'cwd' in popen_args:
+        cwd = popen_args.pop('cwd')
+        if not os.path.isabs(cwd):
+            popen_args['cwd'] = support_file(cwd)
+    process = subprocess.Popen(
+        ['python', '-m', 'nose2.__main__'] + list(nose2_args),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        **popen_args)
+    output, err = process.communicate()
+    retcode = process.poll()
+    return retcode, output, err

46ab40fd8069868ba82e8ce47abe21f08963d486
jpellerin@gmail.com <jpellerin@gmail.com>
2011-12-28 16:27:07 -0500

    Add first parts of pluggable test loader and tests

---
+++ nose2/loader.py
@@ -0,0 +1,27 @@
+from nose2 import events
+from nose2.compat import unittest
+
+
+class PluggableTestLoader(object):
+    """Test loader that defers all loading to plugins"""
+
+    suiteClass = unittest.TestSuite
+
+    def __init__(self, session):
+        self.session = session
+
+    def loadTestsFromModule(self, module):
+        evt = self.session.event(
+            events.LoadFromModuleEvent,
+            self, module)
+        self.session.hooks.loadTestsFromModule(evt)
+        return self.suiteClass(evt.extraTests)
+
+    def loadTestsFromNames(self, testNames, module=None):
+        evt = self.session.event(
+            events.LoadFromNamesEvent,
+            self,
+            testNames,
+            module)
+        self.session.hooks.loadTestsFromNames(evt)
+        return self.suiteClass(evt.extraTests)

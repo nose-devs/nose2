98f641fd3bcc49d790dbe9094b38dca04755f129
jpellerin@gmail.com <jpellerin@gmail.com>
2011-12-28 16:43:30 -0500

    Add initial plugin interface skeleton
    - Add Hooks class
    - Add PluginInterface Class
    - Add initial set of Event classes

--- nose2/events.py
+++ nose2/events.py
@@ -43,14 +43,83 @@ class Plugin(six.with_metaclass(PluginMeta)):
         pass
 
 
+class Hook(object):
+    def __init__(self, method):
+        self.method = method
+        self.plugins = []
+
+    def __call__(self, event):
+        for plugin in self.plugins[:]:
+            result = getattr(plugin, self.method)(event)
+            if event.handled:
+                return result
+
+    def append(self, plugin):
+        self.plugins.append(plugin)
+
+
+class PluginInterface(object):
+    def __init__(self):
+        self.hooks = {}
+
+    def register(self, method, plugin):
+        self.hooks.setdefault(method, Hook(method)).append(plugin)
+
+    def __getattr__(self, attr):
+        return self.hooks.setdefault(attr, Hook(attr))
+
 
 class Event(object):
-    pass
+    def __init__(self, session=None, **kw):
+        self.handled = False
+        self.session = session
+        self.info = {}
+        super(Event, self).__init__(**kw)
+
+    def message(self, message, verbosity=(1, 2)):
+        if self.session:
+            self.session.message(message, verbosity)
+        log.warning("Unable to send message %s: no session", message)
+
+
+class PluginsLoadedEvent(Event):
+    def __init__(self, pluginsLoaded, **kw):
+        self.pluginsLoaded = pluginsLoaded
+        super(PluginsLoadedEvent, self).__init__(**kw)
 
 
 class StartTestEvent(Event):
     pass
 
+
+class LoadFromModuleEvent(Event):
+    def __init__(self, loader, module, **kw):
+        self.loader = loader
+        self.module = module
+        self.extraTests = []
+        super(LoadFromModuleEvent, self).__init__(**kw)
+
+
+class LoadFromNamesEvent(Event):
+    def __init__(self, loader, names, module, **kw):
+        self.loader = loader
+        self.names = names
+        self.module = module
+        self.extraTests = []
+        super(LoadFromNamesEvent, self).__init__(**kw)
+
+
+class HandleFileEvent(Event):
+    def __init__(self, loader, name, path, pattern,
+                    top_level_directory, **kw):
+        self.extraTests = []
+        self.path = path
+        self.loader = loader
+        self.name = name
+        # note: pattern may be None if not called during test discovery
+        self.pattern = pattern
+        self.top_level_directory = top_level_directory
+        super(HandleFileEvent, self).__init__(**kw)
 
 class TestReport(Event):
     pass

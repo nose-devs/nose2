4564fc8dc6c8bed1611bff1b3dcda5d6f4d8ed5b
jpellerin@gmail.com <jpellerin@gmail.com>
2011-12-28 14:38:43 -0500

    More bones for plugin/events api skeleton

--- nose2/events.py
+++ nose2/events.py
@@ -1,5 +1,6 @@
 import six
 
+from nose2 import config
 
 class PluginMeta(type):
     def __call__(cls, *args, **kwargs):
@@ -10,10 +11,31 @@ class PluginMeta(type):
             configSection = getattr(instance, 'configSection', None)
             if configSection is not None:
                 instance.config = session.get(configSection)
+        else:
+            instance.config = config.Config([])
         instance.__init__(*args, **kwargs)
         return instance
 
 
 class Plugin(six.with_metaclass(PluginMeta)):
+
+    def register(self):
+        """Add myself to the plugins that get called"""
+        pass
+
+    def addOption(self, callback, short_opt, long_opt, help_text=None):
+        """Add command-line option"""
+        pass
+
+
+
+class Event(object):
     pass
 
+
+class StartTestEvent(Event):
+    pass
+
+
+class TestReport(Event):
+    pass

98dfa5be4422b673bc4f86b6bd6dc560ddc0c848
jpellerin@gmail.com <jpellerin@gmail.com>
2011-12-28 16:31:48 -0500

    Added path/name related helpers

--- nose2/util.py
+++ nose2/util.py
@@ -1,3 +1,7 @@
+import os
+import re
+import sys
+
 try:
     from compiler.consts import CO_GENERATOR
 except ImportError:
@@ -12,6 +16,11 @@ except ImportError:
     def isgeneratorfunction(func):
         return bool((inspect.isfunction(func) or inspect.ismethod(func)) and
                     func.func_code.co_flags & CO_GENERATOR)
+
+
+IDENT_RE = re.compile(r'^[_a-zA-Z]\w*$r', re.UNICODE)
+VALID_MODULE_RE = re.compile(r'[_a-zA-Z]\w*\.py$', re.UNICODE)
+
 
 def ln(label, char='-', width=70):
     """Draw a divider, with label in the middle.
@@ -30,3 +39,47 @@ def ln(label, char='-', width=70):
     if pad > 0:
         out = out + (char * pad)
     return out
+
+
+def valid_module_name(path):
+    return VALID_MODULE_RE.search(path)
+
+
+def name_from_path(path):
+    # back up to find module root
+    parts = []
+    path = os.path.normpath(path)
+    base = os.path.splitext(path)[0]
+    candidate, top = os.path.split(base)
+    parts.append(top)
+    while candidate:
+        if ispackage(candidate):
+            candidate, top = os.path.split(candidate)
+            parts.append(top)
+        else:
+            break
+    return '.'.join(reversed(parts))
+
+
+def module_from_name(name):
+    __import__(name)
+    return sys.modules[name]
+
+
+def ispackage(path):
+    """
+    Is this path a package directory?
+
+    """
+    if os.path.isdir(path):
+        # at least the end of the path must be a legal python identifier
+        # and __init__.py[co] must exist
+        end = os.path.basename(path)
+        if IDENT_RE.match(end):
+            for init in ('__init__.py', '__init__.pyc', '__init__.pyo'):
+                if os.path.isfile(os.path.join(path, init)):
+                    return True
+            if sys.platform.startswith('java') and \
+                    os.path.isfile(os.path.join(path, '__init__$py.class')):
+                return True
+    return False

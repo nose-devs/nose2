8a140d8b6215573b233cb7a55fd8e68a98c1b9ef
JP <jpellerin@gmail.com>
2011-12-29 16:15:20 -0500

    Update imports

--- nose2/tests/unit/test_loader_plugin.py
+++ nose2/tests/unit/test_loader_plugin.py
@@ -1,4 +1,4 @@
-from nose2.plugins import loader
+from nose2.plugins.loader import functions
 from nose2.tests._common import TestCase
 
 
@@ -14,12 +14,12 @@ class TestGeneratorUnpack(TestCase):
         def gen():
             for i in range(0, 3):
                 yield 'call', i, i + 1
-        out = list(loader.Functions().unpack(gen()))
+        out = list(functions.Functions().unpack(gen()))
         self.assertEqual(out, self.expect)
 
     def test_unpack_handles_unittest2_style_generators(self):
         def gen():
             for i in range(0, 3):
                 yield 'call', (i, i + 1)
-        out = list(loader.Functions().unpack(gen()))
+        out = list(functions.Functions().unpack(gen()))
         self.assertEqual(out, self.expect)

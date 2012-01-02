277eca85f5eaf009c9ae7a38ef28801a747f9efa
JP <jpellerin@gmail.com>
2012-01-02 14:06:02 -0500

    Add copyright notice

--- nose2/util.py
+++ nose2/util.py
@@ -1,3 +1,11 @@
+"""
+This module contains some code copied from unittest2/loader.py and other
+code developed in reference to that module and others within unittest2.
+
+unittest2 is Copyright (c) 2001-2010 Python Software Foundation; All
+Rights Reserved. See: http://docs.python.org/license.html
+
+"""
 import os
 import re
 import sys

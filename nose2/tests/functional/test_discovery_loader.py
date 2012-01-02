9be89f6f9e6bbd0bca29e9dc3c09d5a9e6e9e36a
JP <jpellerin@gmail.com>
2012-01-02 14:05:47 -0500

    Add more discovery tests

--- nose2/tests/functional/test_discovery_loader.py
+++ nose2/tests/functional/test_discovery_loader.py
@@ -1,4 +1,4 @@
-from nose2.tests._common import FunctionalTestCase, support_file
+from nose2.tests._common import FunctionalTestCase, TestCase, support_file
 from nose2 import events, loader, session
 from nose2.plugins.loader.discovery import DiscoveryLoader
 
@@ -16,13 +16,53 @@ class DiscoveryFunctionalTest(FunctionalTestCase):
         self.session = session.Session()
         self.plug = DiscoveryLoader(session=self.session)
         self.loader = loader.PluggableTestLoader(self.session)
+        self.watcher = Watcher(session=self.session)
+        self.watcher.register()
 
-    def test_createTests_hook(self):
+    def test_can_discover_test_modules_in_packages(self):
         self.plug.start_dir = support_file('scenario/tests_in_package')
-        watcher = Watcher(session=self.session)
-        watcher.register()
         event = events.CreateTestsEvent(self.loader, None, None)
         result = self.session.hooks.createTests(event)
         assert isinstance(result, self.loader.suiteClass)
-        assert watcher.called
+        self.assertEqual(len(self.watcher.called), 1)
+        self.assertEqual(self.watcher.called[0].module.__name__,
+                         'pkg1.test.test_things')
+
+    def test_discovery_supports_code_in_lib_dir(self):
+        self.plug.start_dir = support_file('scenario/package_in_lib')
+        event = events.CreateTestsEvent(self.loader, None, None)
+        result = self.session.hooks.createTests(event)
+        assert isinstance(result, self.loader.suiteClass)
+        self.assertEqual(len(self.watcher.called), 1)
+        self.assertEqual(self.watcher.called[0].module.__name__, 'tests')
+
+    def test_match_path_event_can_prevent_discovery(self):
+        class NoTestsForYou(events.Plugin):
+            def matchPath(self, event):
+                event.handled = True
+                return False
+        mp = NoTestsForYou(session=self.session)
+        mp.register()
+        self.plug.start_dir = support_file('scenario/tests_in_package')
+        event = events.CreateTestsEvent(self.loader, None, None)
+        result = self.session.hooks.createTests(event)
+        assert isinstance(result, self.loader.suiteClass)
+        self.assertEqual(len(self.watcher.called), 0)
+
+    def test_handle_file_event_can_add_tests(self):
+        class TextTest(TestCase):
+            def test(self):
+                pass
+        class TestsInText(events.Plugin):
+            def handleFile(self, event):
+                if event.path.endswith('.txt'):
+                    event.extraTests.append(TextTest('test'))
+        mp = TestsInText(session=self.session)
+        mp.register()
+        self.plug.start_dir = support_file('scenario/tests_in_package')
+        event = events.CreateTestsEvent(self.loader, None, None)
+        result = self.session.hooks.createTests(event)
+        assert isinstance(result, self.loader.suiteClass)
+        self.assertEqual(len(result._tests), 2)
+        self.assertEqual(len(self.watcher.called), 1)
 

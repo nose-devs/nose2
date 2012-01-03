4f70897d5a85f1822a93df9bc91979ea79594901
JP <jpellerin@gmail.com>
2012-01-03 15:38:15 -0500

    Add initial tests for generators plugin

--- nose2/tests/unit/test_generators_plugin.py
+++ nose2/tests/unit/test_generators_plugin.py
@@ -1,4 +1,5 @@
-from nose2.plugins.loader import functions
+from nose2 import events, loader, session
+from nose2.plugins.loader import generators
 from nose2.tests._common import TestCase
 
 
@@ -6,20 +7,48 @@ class TestGeneratorUnpack(TestCase):
     tags = ['unit']
 
     def setUp(self):
+        self.session = session.Session()
+        self.loader = loader.PluggableTestLoader(self.session)
         self.expect = [(0, ('call', (0, 1))),
                        (1, ('call', (1, 2))),
                        (2, ('call', (2, 3))),]
+        self.plugin = generators.Generators(session=self.session)
 
     def test_unpack_handles_nose_style_generators(self):
         def gen():
             for i in range(0, 3):
                 yield 'call', i, i + 1
-        out = list(functions.Functions().unpack(gen()))
+        out = list(self.plugin.unpack(gen()))
         self.assertEqual(out, self.expect)
 
     def test_unpack_handles_unittest2_style_generators(self):
         def gen():
             for i in range(0, 3):
                 yield 'call', (i, i + 1)
-        out = list(functions.Functions().unpack(gen()))
+        out = list(self.plugin.unpack(gen()))
         self.assertEqual(out, self.expect)
+
+    def test_ignores_ordinary_functions(self):
+        class Mod(object):
+            pass
+        def test():
+            pass
+        m = Mod()
+        m.test = test
+        event = events.LoadFromModuleEvent(self.loader, m)
+        self.session.hooks.loadTestsFromModule(event)
+        self.assertEqual(len(event.extraTests), 0)
+
+    def test_can_load_tests_from_generator_functions(self):
+        class Mod(object):
+            pass
+        def check(x):
+            assert x == 1
+        def test():
+            yield check, 1
+            yield check, 2
+        m = Mod()
+        m.test = test
+        event = events.LoadFromModuleEvent(self.loader, m)
+        self.session.hooks.loadTestsFromModule(event)
+        self.assertEqual(len(event.extraTests), 2)

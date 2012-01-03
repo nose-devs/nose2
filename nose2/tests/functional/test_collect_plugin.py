d6da60e40658fd3b602b35c3e18d7e7214a445d2
JP <jpellerin@gmail.com>
2012-01-03 15:47:31 -0500

    Make collect plugin func test more robust
    Collect plugin does not care about test order, only that all tests are
    found and all "pass".

--- nose2/tests/functional/test_collect_plugin.py
+++ nose2/tests/functional/test_collect_plugin.py
@@ -13,35 +13,6 @@ class CollectOnlyFunctionalTest(FunctionalTestCase):
 # expectations
 
 EXPECT_LAYOUT1 = re.compile("""\
-test_failed \(pkg1\.test\.test_things\.SomeTests\) \.\.\. ok
-test_ok \(pkg1\.test\.test_things\.SomeTests\) \.\.\. ok
-test_skippy \(pkg1\.test\.test_things\.SomeTests\) \.\.\. ok
-test_typeerr \(pkg1\.test\.test_things\.SomeTests\) \.\.\. ok
-unittest2\.case\.FunctionTestCase \(test_fixt\) \.\.\. ok
-unittest2\.case\.FunctionTestCase \(test_func\) \.\.\. ok
-pkg1\.test\.test_things\.test_gen:1
-0, 0 \.\.\. ok
-pkg1\.test\.test_things\.test_gen:2
-1, 1 \.\.\. ok
-pkg1\.test\.test_things\.test_gen:3
-2, 2 \.\.\. ok
-pkg1\.test\.test_things\.test_gen:4
-3, 3 \.\.\. ok
-pkg1\.test\.test_things\.test_gen:5
-4, 4 \.\.\. ok
-pkg1\.test\.test_things\.test_gen_nose_style:1
-0, 0 \.\.\. ok
-pkg1\.test\.test_things\.test_gen_nose_style:2
-1, 1 \.\.\. ok
-pkg1\.test\.test_things\.test_gen_nose_style:3
-2, 2 \.\.\. ok
-pkg1\.test\.test_things\.test_gen_nose_style:4
-3, 3 \.\.\. ok
-pkg1\.test\.test_things\.test_gen_nose_style:5
-4, 4 \.\.\. ok
-
-----------------------------------------------------------------------
 Ran 16 tests in \d.\d+s
 
-OK
-""")
+OK""")

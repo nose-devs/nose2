2c85d3c1b83475baa0832ab28dcbdc2b2091be87
JP <jpellerin@gmail.com>
2012-01-03 12:04:29 -0500

    Implement pluggable test result

--- nose2/result.py
+++ nose2/result.py
@@ -1,38 +1,69 @@
-from nose2.compat import unittest
+import time
+
 from nose2 import events
 
 
-class PluggableTestResult(unittest.TestResult):
+ERROR = 'error'
+FAIL = 'failed'
+SKIP = 'skipped'
+PASS = 'passed'
+
+
+class PluggableTestResult(object):
+    """Test result that defers to plugins.
+
+    All test outcome recording and reporting is deferred to plugins,
+    which are expected to implement startTest, stopTest, testOutcome,
+    and wasSuccessful.
+
+    """
     def __init__(self, session):
         self.session = session
         self.shouldStop = False
 
     def startTest(self, test):
-        pass
+        event = events.StartTestEvent(test, self, time.time())
+        self.session.hooks.startTest(event)
 
     def stopTest(self, test):
-        pass
+        event = events.StopTestEvent(test, self, time.time())
+        self.session.hooks.stopTest(event)
 
     def addError(self, test, err):
-        pass
+        event = events.TestOutcomeEvent(test, self, ERROR, err)
+        self.session.hooks.testOutcome(event)
 
     def addFailure(self, test, err):
-        pass
+        event = events.TestOutcomeEvent(test, self, FAIL, err)
+        self.session.hooks.testOutcome(event)
 
     def addSuccess(self, test):
-        pass
+        event = events.TestOutcomeEvent(test, self, PASS)
+        self.session.hooks.testOutcome(event)
 
     def addSkip(self, test, reason):
-        pass
+        event = events.TestOutcomeEvent(test, self, SKIP)
+        self.session.hooks.testOutcome(event)
 
     def addExpectedFailure(self, test, err):
-        pass
+        event = events.TestOutcomeEvent(test, self, FAIL, err, expected=True)
+        self.session.hooks.testOutcome(event)
 
     def addUnexpectedSuccess(self, test):
-        pass
+        event = events.TestOutcomeEvent(test, self, PASS, expected=False)
+        self.session.hooks.testOutcome(event)
 
     def wasSuccessful(self):
-        return True
+        # assume failure, plugins must affirmatively declare success
+        try:
+            return self._success
+        except AttributeError:
+            event = events.ResultSuccessEvent(self, False)
+            self.session.hooks.wasSuccessful(event)
+            self._success = event.success
+            return self._success
 
     def stop(self):
-        self.shouldStop = True
+        event = events.ResultStopEvent(self, True)
+        self.session.hooks.resultStop(event)
+        self.shouldStop = event.shouldStop

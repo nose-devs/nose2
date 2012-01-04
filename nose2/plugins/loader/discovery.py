7e42858ab833d42fa5831eb8a7ac88ac09a41bad
JP <jpellerin@gmail.com>
2012-01-04 11:59:58 -0500

    Rewrote discovery
    - More modular implementation
    - Support for discovery from dirs, files, modules and packages both in
      full-on discover mode and in load from name mode

--- nose2/plugins/loader/discovery.py
+++ nose2/plugins/loader/discovery.py
@@ -9,218 +9,187 @@ Rights Reserved. See: http://docs.python.org/license.html
 
 """
 from fnmatch import fnmatch
+import logging
 import os
 import sys
 
 from nose2 import events, util
 
 __unittest = True
+log = logging.getLogger(__name__)
 
 
 class DiscoveryLoader(events.Plugin):
+    """Loader plugin that can discover tests
 
+    Responds to loadTestsFromNames when names and module are blank
+    by discovering tests in the configured start directory of the
+    session.
+
+    Responds to loadTestsFromName when the name is a module, file
+    or directory.
+
+    """
     def __init__(self):
-        self.start_dir = None
-        self.top_level_dir = self.start_dir
-        self._top_level_dir = None # ???
-
-        self.addArgument(
-            self.setStartDir,
-            'S', 'start-directory',
-            "Directory to start discovery ('.' default)")
-        self.addArgument(
-            self.setTopLevelDir,
-            'T', 'top-level-directory',
-            "Top level directory of project (defaults to start dir)")
         # really always on!
         self.register()
 
-    def setStartDir(self, start_dir):
-        if isinstance(start_dir, list):
-            start_dir = start_dir[0]
-        self.start_dir = start_dir
+    def loadTestsFromName(self, event):
+        # turn name into path or module name
+        # fire appropriate hooks (handle file or load from module)
+        if event.module:
+            return
+        name = event.name
+        module = None
+        _, top_level_dir = self._getStartDirs()
+        try:
+            # try name as a dotted module name first
+            __import__(name)
+            module = sys.modules[name]
+        except ImportError:
+            # if that fails, try it as a file or directory
+            event.extraTests.extend(
+                self._find_tests(event, name, top_level_dir))
+        else:
+            event.extraTests.extend(
+                self._find_tests_in_module(event, module, top_level_dir))
 
-    def setTopLevelDir(self, top_level_dir):
-        if isinstance(top_level_dir, list):
-            top_level_dir = top_level_dir[0]
-        self.top_level_dir = top_level_dir
+    def loadTestsFromNames(self, event):
+        log.debug("Received event %s", event)
+        if event.names or event.module:
+            return
+        event.handled = True # I will handle discovery
+        return self._discover(event)
 
-    def createTests(self, event):
-        """Discover tests in my start_dir.
-
-        .. note ::
-
-           Sets event.handled to True, which prevents other plugins
-           from responding to this event. Other plugins that want
-           to load tests should implement handleFile or
-           loadTestsFromModule.
-
-        """
-        event.handled = True # I'm taking this one
-        return self._discover(event, self.start_dir, self.top_level_dir)
-
-    def _discover(self, event, start_dir, top_level_dir):
-        loader = event.loader
-        pattern = self.session.testFilePattern
+    def _getStartDirs(self):
+        start_dir = self.session.startDir
+        top_level_dir = self.session.topLevelDir
         if start_dir is None:
             start_dir = '.'
-        set_implicit_top = False
-        if top_level_dir is None and self._top_level_dir is not None:
-            # make top_level_dir optional if called from load_tests in a package
-            top_level_dir = self._top_level_dir
-        elif top_level_dir is None:
-            set_implicit_top = True
+        if top_level_dir is None:
             top_level_dir = start_dir
 
-        top_level_dir = os.path.abspath(top_level_dir)
-
-        if not top_level_dir in sys.path:
-            # all test modules must be importable from the top level directory
-            # should we *unconditionally* put the start directory in first
-            # in sys.path to minimise likelihood of conflicts between installed
-            # modules and development versions?
-            sys.path.insert(0, top_level_dir)
-        self._top_level_dir = top_level_dir
+        if not os.path.isdir(os.path.abspath(start_dir)):
+            raise OSError("%s is not a directory" % start_dir)
 
         is_not_importable = False
-        if os.path.isdir(os.path.abspath(start_dir)):
-            started_with_dir = True
-            start_dir = os.path.abspath(start_dir)
-            if start_dir != top_level_dir:
-                is_not_importable = not os.path.isfile(
-                    os.path.join(start_dir, '__init__.py'))
-        else:
-            # support for discovery from dotted module names
-            started_with_dir = False
-            try:
-                __import__(start_dir)
-            except ImportError:
-                is_not_importable = True
-            else:
-                the_module = sys.modules[start_dir]
-                top_part = start_dir.split('.')[0]
-                start_dir = os.path.abspath(
-                    os.path.dirname((the_module.__file__)))
-                if set_implicit_top:
-                    self._top_level_dir = os.path.abspath(
-                        os.path.dirname(
-                            os.path.dirname(sys.modules[top_part].__file__)))
-                    sys.path.remove(top_level_dir)
-
+        start_dir = os.path.abspath(start_dir)
+        top_level_dir = os.path.abspath(top_level_dir)
+        if start_dir != top_level_dir:
+            is_not_importable = not os.path.isfile(
+                os.path.join(start_dir, '__init__.py'))
         if is_not_importable:
             raise ImportError(
                 'Start directory is not importable: %r' % start_dir)
+        # this is redundant in some cases, but that's ok
+        self.session.prepareSysPath()
+        return start_dir, top_level_dir
 
-        def check_dir(the_dir):
-            full_path =  os.path.join(start_dir, the_dir)
-            if (os.path.isdir(full_path) and not
-                os.path.isfile(os.path.join(full_path, '__init__.py'))):
-                sys.path.append(full_path)
-                return full_path
-
-        src_dir = check_dir('src')
-        lib_dir = check_dir('lib')
-        tests = list(self._find_tests(event, start_dir, pattern))
-        real_top_level = self._top_level_dir
-        if lib_dir is not None:
-            self._top_level_dir = lib_dir
-            tests.extend(list(self._find_tests(event, lib_dir, pattern)))
-        if src_dir is not None:
-            self._top_level_dir = src_dir
-            tests.extend(list(self._find_tests(event, src_dir, pattern)))
-        if started_with_dir:
-            for entry in os.listdir(start_dir):
-                if not 'test' in entry.lower():
-                    continue
-                full = check_dir(entry)
-                if full is None:
-                    continue
-                self._top_level_dir = full
-                tests.extend(list(self._find_tests(event, full, pattern)))
-
-        self._top_level_dir = real_top_level
+    def _discover(self, event):
+        loader = event.loader
+        try:
+            start_dir, top_level_dir = self._getStartDirs()
+        except (OSError, ImportError):
+            _, ev, _ = sys.exc_info()
+            return loader.suiteClass(
+                loader.failedLoadTests(self.session.startDir, ev))
+        log.debug("_discover in %s (%s)", start_dir, top_level_dir)
+        tests = list(self._find_tests(event, start_dir, top_level_dir))
         return loader.suiteClass(tests)
 
-    def _find_tests(self, event, start_dir, pattern):
+    def _find_tests(self, event, start, top_level):
         """Used by discovery. Yields test suites it loads."""
-        paths = os.listdir(start_dir)
+        log.debug('_find_tests(%r, %r)', start, top_level)
+        if start == top_level:
+            full_path = start
+        else:
+            full_path = os.path.join(top_level, start)
+        if os.path.isdir(start):
+            for test in self._find_tests_in_dir(
+                event, full_path, top_level):
+                yield test
+        elif os.path.isfile(start):
+            for test in self._find_tests_in_file(
+                event, start, full_path, top_level):
+                yield test
 
-        for path in paths:
-            full_path = os.path.join(start_dir, path)
-            if os.path.isfile(full_path):
-                evt = events.HandleFileEvent(self, path, full_path, pattern,
-                                             self._top_level_dir)
-                result = self.session.hooks.handleFile(evt)
-                if evt.extraTests:
-                    yield event.loader.suiteClass(evt.extraTests)
-
-                if evt.handled:
-                    if result:
-                        yield result
-                    continue
-
-                if not util.valid_module_name(path):
-                    # valid Python identifiers only
-                    continue
-
-                evt = events.MatchPathEvent(path, full_path, pattern)
-                result = self.session.hooks.matchPath(evt)
-                if evt.handled:
-                    if not result:
-                        continue
-                elif not self._match_path(path, full_path, pattern):
-                    continue
-
-                # if the test file matches, load it
-                name = util.name_from_path(full_path)
-                try:
-                    module = util.module_from_name(name)
-                except:
-                    yield event.loader.failedImport(name)
-                else:
-                    mod_file = os.path.abspath(
-                        getattr(module, '__file__', full_path))
-                    realpath = os.path.splitext(mod_file)[0]
-                    fullpath_noext = os.path.splitext(full_path)[0]
-                    if realpath.lower() != fullpath_noext.lower():
-                        module_dir = os.path.dirname(realpath)
-                        mod_name = os.path.splitext(os.path.basename(full_path))[0]
-                        expected_dir = os.path.dirname(full_path)
-                        msg = ("%r module incorrectly imported from %r. "
-                               "Expected %r. Is this module globally installed?"
-                               )
-                        raise ImportError(
-                            msg % (mod_name, module_dir, expected_dir))
-                    yield event.loader.loadTestsFromModule(module)
-            elif os.path.isdir(full_path):
-                if not os.path.isfile(os.path.join(full_path, '__init__.py')):
-                    continue
-
-                load_tests = None
-                tests = None
-                if fnmatch(path, pattern):
-                    # only check load_tests if the package directory
-                    # itself matches the filter
-                    name = util.name_from_path(full_path)
-                    package = util.module_from_name(name)
-                    load_tests = getattr(package, 'load_tests', None)
-                    tests = event.loader.loadTestsFromModule(
-                        package, use_load_tests=False)
-
-                if load_tests is None:
-                    if tests is not None:
-                        # tests loaded from package file
-                        yield tests
-                    # recurse into the package
-                    for test in self._find_tests(event, full_path, pattern):
+    def _find_tests_in_dir(self, event, full_path, top_level):
+        log.debug("find in dir %s (%s)", full_path, top_level)
+        for path in os.listdir(full_path):
+            entry_path = os.path.join(full_path, path)
+            if os.path.isfile(entry_path):
+                for test in self._find_tests_in_file(
+                    event, path, entry_path, top_level):
+                    yield test
+            elif os.path.isdir(entry_path):
+                # FIXME need a hook or config opt here to allow
+                # plugins/config to add more dirnames to look in
+                if ('test' in path.lower()
+                    or util.ispackage(entry_path)
+                    or path in self.session.libDirs):
+                    for test in self._find_tests(event, entry_path, top_level):
                         yield test
-                else:
-                    try:
-                        yield load_tests(self, tests, pattern)
-                    except Exception:
-                        ec, ev, tb = sys.exec_info()
-                        yield event.loader.failedLoadTests(
-                            package.__name__, ev)
+
+    def _find_tests_in_file(self, event, filename, full_path, top_level):
+        log.debug("find in file %s (%s)", full_path, top_level)
+        pattern = self.session.testFilePattern
+        loader = event.loader
+        evt = events.HandleFileEvent(
+            loader, filename, full_path, pattern, top_level)
+        result = self.session.hooks.handleFile(evt)
+        if evt.extraTests:
+            yield loader.suiteClass(evt.extraTests)
+
+        if evt.handled:
+            if result:
+                yield result
+            return
+
+        if not util.valid_module_name(filename):
+            # valid Python identifiers only
+            return
+
+        evt = events.MatchPathEvent(filename, full_path, pattern)
+        result = self.session.hooks.matchPath(evt)
+        if evt.handled:
+            if not result:
+                return
+        elif not self._match_path(filename, full_path, pattern):
+            return
+
+        # if the test file matches, load it
+        name = util.name_from_path(full_path)
+        try:
+            module = util.module_from_name(name)
+        except:
+            yield loader.failedImport(name)
+        else:
+            mod_file = os.path.abspath(
+                getattr(module, '__file__', full_path))
+            realpath = os.path.splitext(mod_file)[0]
+            fullpath_noext = os.path.splitext(full_path)[0]
+            if realpath.lower() != fullpath_noext.lower():
+                module_dir = os.path.dirname(realpath)
+                mod_name = os.path.splitext(os.path.basename(full_path))[0]
+                expected_dir = os.path.dirname(full_path)
+                msg = ("%r module incorrectly imported from %r. "
+                       "Expected %r. Is this module globally installed?"
+                       )
+                raise ImportError(
+                    msg % (mod_name, module_dir, expected_dir))
+            yield loader.loadTestsFromModule(module)
+
+    def _find_tests_in_module(self, event, module, top_level_dir):
+        # only called from loadTestsFromName
+        yield event.loader.loadTestsFromModule(module)
+        # may be a package; recurse into __path__ if so
+        pkgpath = getattr(module, '__path__', None)
+        if pkgpath:
+            for entry in pkgpath:
+                full_path = os.path.abspath(os.path.join(top_level_dir, entry))
+                for test in self._find_tests_in_dir(
+                    event, full_path, top_level_dir):
+                    yield test
 
     def _match_path(self, path, full_path, pattern):
         # override this method to use alternative matching strategy

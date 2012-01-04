e6d28d55309cdf7c25062d469646e0671e877607
JP <jpellerin@gmail.com>
2012-01-04 16:50:53 -0500

    Add param test cases to func test target project

--- nose2/tests/functional/support/scenario/tests_in_package/pkg1/test/test_things.py
+++ nose2/tests/functional/support/scenario/tests_in_package/pkg1/test/test_things.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class SomeTests(unittest.TestCase):
 
     def test_ok(self):
@@ -14,6 +15,16 @@ class SomeTests(unittest.TestCase):
 
     def test_skippy(self):
         raise unittest.SkipTest("I wanted to skip")
+
+    def test_gen_method(self):
+        def check(x):
+            assert x == 1
+        yield check, 1
+        yield check, 2
+
+    def test_params_method(self, a):
+        self.assertEqual(a, 1)
+    test_params_method.paramList = (1, 2)
 
 
 def test_func():
@@ -45,3 +56,12 @@ def test_fixt():
     assert did_setup
 test_fixt.setup = setup
 
+
+def test_params_func(a):
+    assert a == 1
+test_params_func.paramList = (1, 2)
+
+
+def test_params_func_multi_arg(a, b):
+    assert a == b
+test_params_func_multi_arg.paramList = ((1, 1), (1, 2), (2, 2))

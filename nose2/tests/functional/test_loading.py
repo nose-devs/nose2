4ccf50629108513737cec529f298fb0865f39e2b
JP <jpellerin@gmail.com>
2012-01-04 12:10:18 -0500

    Decode proc output before checking (py32 fix)

--- nose2/tests/functional/test_loading.py
+++ nose2/tests/functional/test_loading.py
@@ -18,7 +18,7 @@ pkg1.test.test_things.SomeTests.test_ok
 
 """
 from nose2.tests._common import FunctionalTestCase, support_file
-
+from nose2 import util
 
 class TestLoadTestsFromPackage(FunctionalTestCase):
     def test_module_name(self):
@@ -28,7 +28,7 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'Ran 16 tests' in stderr, stderr
+        assert 'Ran 16 tests' in util.safe_decode(stderr), stderr
 
     def test_package_name(self):
         proc = self.runIn(
@@ -37,7 +37,7 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'Ran 16 tests' in stderr, stderr
+        assert 'Ran 16 tests' in util.safe_decode(stderr), stderr
 
     def test_module_name_with_start_dir(self):
         proc = self.runIn(
@@ -45,14 +45,14 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'Ran 16 tests' in stderr, stderr
+        assert 'Ran 16 tests' in util.safe_decode(stderr), stderr
 
     def test_package_name_with_start_dir(self):
         proc = self.runIn(
             '.', '-v', '-s', support_file('scenario/tests_in_package'), 'pkg1')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'Ran 16 tests' in stderr, stderr
+        assert 'Ran 16 tests' in util.safe_decode(stderr), stderr
 
     def test_function_name(self):
         proc = self.runIn(
@@ -61,9 +61,9 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things.test_func')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'test_func' in stderr
-        assert 'Ran 1 test' in stderr
-        assert 'OK' in stderr
+        assert 'test_func' in util.safe_decode(stderr), stderr
+        assert 'Ran 1 test' in util.safe_decode(stderr), stderr
+        assert 'OK' in util.safe_decode(stderr), stderr
 
     def test_generator_function_name(self):
         proc = self.runIn(
@@ -72,8 +72,8 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things.test_gen')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'test_gen' in stderr
-        assert 'Ran 5 tests' in stderr
+        assert 'test_gen' in util.safe_decode(stderr), stderr
+        assert 'Ran 5 tests' in util.safe_decode(stderr), stderr
 
     def test_generator_function_index(self):
         proc = self.runIn(
@@ -82,8 +82,8 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things.test_gen:3')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'test_gen' in stderr
-        assert 'Ran 1 test' in stderr
+        assert 'test_gen' in util.safe_decode(stderr), stderr
+        assert 'Ran 1 test' in util.safe_decode(stderr), stderr
 
     def test_generator_function_index_1_based(self):
         proc = self.runIn(
@@ -92,9 +92,9 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things.test_gen:1')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'test_gen' in stderr
-        assert 'Ran 1 test' in stderr
-        assert 'OK' in stderr
+        assert 'test_gen' in util.safe_decode(stderr), stderr
+        assert 'Ran 1 test' in util.safe_decode(stderr), stderr
+        assert 'OK' in util.safe_decode(stderr), stderr
 
     def test_testcase_name(self):
         proc = self.runIn(
@@ -103,8 +103,8 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things.SomeTests')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'SomeTests' in stderr, stderr
-        assert 'Ran 4 tests' in stderr, stderr
+        assert 'SomeTests' in util.safe_decode(stderr), stderr
+        assert 'Ran 4 tests' in util.safe_decode(stderr), stderr
 
     def test_testcase_method(self):
         proc = self.runIn(
@@ -113,9 +113,9 @@ class TestLoadTestsFromPackage(FunctionalTestCase):
             'pkg1.test.test_things.SomeTests.test_ok')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'SomeTests' in stderr, stderr
-        assert 'Ran 1 test' in stderr, stderr
-        assert 'OK' in stderr, stderr
+        assert 'SomeTests' in util.safe_decode(stderr), stderr
+        assert 'Ran 1 test' in util.safe_decode(stderr), stderr
+        assert 'OK' in util.safe_decode(stderr), stderr
 
 
 class TestLoadTestsOutsideOfPackage(FunctionalTestCase):
@@ -126,7 +126,7 @@ class TestLoadTestsOutsideOfPackage(FunctionalTestCase):
             'tests')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'Ran 3 tests' in stderr, stderr
+        assert 'Ran 3 tests' in util.safe_decode(stderr), stderr
 
     def test_function_name(self):
         proc = self.runIn(
@@ -135,13 +135,13 @@ class TestLoadTestsOutsideOfPackage(FunctionalTestCase):
             'tests.test')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'tests.test' in stderr
-        assert 'Ran 1 test' in stderr
-        assert 'OK' in stderr
+        assert 'tests.test' in util.safe_decode(stderr), stderr
+        assert 'Ran 1 test' in util.safe_decode(stderr), stderr
+        assert 'OK' in util.safe_decode(stderr), stderr
 
     def test_module_name_with_start_dir(self):
         proc = self.runIn(
             '.', '-v', '-s', support_file('scenario/package_in_lib'), 'tests')
         stdout, stderr = proc.communicate()
         self.assertEqual(proc.poll(), 0, stderr)
-        assert 'Ran 3 tests' in stderr, stderr
+        assert 'Ran 3 tests' in util.safe_decode(stderr), stderr

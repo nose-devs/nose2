7c6b202adbeccd7184219bcf2eb6d782bdbadb2f
JP <jpellerin@gmail.com>
2012-01-30 11:24:30 -0500

    Add docstring for nose2.util.transplant_class

--- nose2/util.py
+++ nose2/util.py
@@ -193,6 +193,16 @@ def format_traceback(test, err):
 
 
 def transplant_class(cls, module):
+    """Make class appear to reside in ``module``.
+
+    :param cls: A class
+    :param module: A module name
+    :returns: A subclass of ``cls`` that appears to have been defined in ``module``. 
+
+    The returned class's ``__name__`` will be equal to
+    ``cls.__name__``, and its ``__module__`` equal to ``module``.
+
+    """
     class C(cls):
         pass
     C.__module__ = module

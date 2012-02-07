7d58ee483cec534c7dea35b2c2ee08219a65135c
JP <jpellerin@gmail.com>
2012-02-07 15:22:07 -0500

    Next step in mp work (in progress)

--- nose2/plugins/mp.py
+++ nose2/plugins/mp.py
@@ -1,9 +1,9 @@
 import logging
 import multiprocessing
-import time
+import select
 import unittest
 
-from nose2 import events
+from nose2 import events, loader, result, runner, session, util
 
 log = logging.getLogger(__name__)
 
@@ -31,27 +31,45 @@ class MultiProcess(events.Plugin):
     def _runmp(self, test, result):
         flat = iter(self._flatten(test))
         procs = self._startProcs()
+
         # distribute tests more-or-less evenly among processes
-        self.results = {}
+        self.cases = {}
         for caseid, case in flat:
             for proc, conn in procs:
-                self.results[caseid] = None
+                # XXX this is wrong, we need to record each
+                # individual case and these could be classes or modules
+                self.cases[caseid] = case
                 conn.send(caseid)
                 caseid = flat.next()
-        # poll pipes for test results
-        while self._waiting():
-            for proc, conn in procs:
+
+        # None is the 'done' flag
+        for proc, conn in procs:
+            conn.send(None)
+
+        # wait for results
+        rdrs = [conn
+                for proc, conn in procs
+                if proc.is_alive()]
+
+        while rdrs:
+            ready, _, _ = select.select(rdrs, [], [], self.testRunTimeout)
+            for conn in ready:
                 # XXX proc could be dead
-                # XXX have a timeout of 1 second once per loop
-                # to avoid busy waiting?
-                if conn.poll():
+                try:
                     remote_events = conn.recv()
-                    # XXX
-                    # replay result calls into my result
-                    for (hook, event) in remote_events:
-                        self._localize(event)
-                        getattr(self.session.hooks, hook)(event)
-            time.sleep(.1)
+                except EOFError:
+                    # probably dead
+                    log.warning("Subprocess connection closed unexpectedly")
+                    continue # XXX or die?
+                # replay events
+                for (hook, event) in remote_events:
+                    self._localize(event)
+                    getattr(self.session.hooks, hook)(event)
+            rdrs = [conn
+                    for proc, conn in procs
+                    if proc.is_alive()]
+        for proc, conn in procs:
+            conn.close()
 
     def _startProcs(self):
         # XXX create session export
@@ -61,6 +79,7 @@ class MultiProcess(events.Plugin):
             parent_conn, child_conn = multiprocessing.Pipe()
             proc = multiprocessing.Process(
                 target=procserver, args=(session_export, child_conn))
+            proc.daemon = True
             proc.start()
             procs.append((proc, parent_conn))
         return procs
@@ -70,12 +89,14 @@ class MultiProcess(events.Plugin):
         # really: examine suite tests to find out if they have class
         # or module fixtures and group them that way into names
         # of test classes or modules
+        # ALSO record all test cases in self._cases
         flat = []
         for test in suite:
             if isinstance(test, unittest.TestSuite):
                 flat.extend(self._flatten(test))
             else:
                 flat.append(test.id())
+                self.cases[test.id()] = test
         return flat
 
     def _waiting(self):
@@ -88,7 +109,15 @@ class MultiProcess(events.Plugin):
         # XXX set loader, case, result etc to local ones, if present in event
         # (event case will be just the id)
         # (traceback in exc_info if any won't be real!)
-        pass
+        if hasattr(event, 'result'):
+            event.result = self.session.testResult
+        if hasattr(event, 'loader'):
+            event.loaded = self.session.testLoader
+        if hasattr(event, 'runner'):
+            event.runner = self.session.testRunner
+        if hasattr(event, 'case'):
+            # remote event.case is the test id
+            event.case = self.cases[event.case]
 
     def _exportSession(self):
         # argparse isn't pickleable
@@ -98,64 +127,136 @@ class MultiProcess(events.Plugin):
                   'verbosity': self.session.verbosity,
                   'startDir': self.session.startDir,
                   'topLevelDir': self.session.topLevelDir,
+                  'logLevel': self.session.logLevel,
                   # XXX classes or modules?
                   'pluginClasses': []}
         # XXX fire registerInSubprocess -- add those plugin classes
         # (classes must be pickleable!)
+        event = RegisterInSubprocessEvent() # FIXME should be own event type
+        self.session.hooks.registerInSubprocess(event)
+        export['pluginClasses'].extend(event.pluginClasses)
         return export
 
 
 def procserver(session_export, conn):
     rlog = logging.getLogger('%s:remote' % __name__)
     # make a real session from the "session" we got
+    ssn = session.Session()
+    ssn.argparse = None # FIXME
+    ssn.pluginargs = None # FIXME
+    ssn.config = session_export['config']
+    ssn.hooks = events.RecordingPluginInterface()
+    ssn.verbosity = session_export['verbosity']
+    ssn.startDir = session_export['startDir']
+    ssn.topLevelDir = session_export['topLevelDir']
+
     # init logging system
-    # loader, result
+    logging.basicConfig(level=session_export['logLevel'])
+    loader_ = loader.PluggableTestLoader(ssn)
+    ssn.testLoader = loader_
+    result_ = result.PluggableTestResult(ssn)
+    ssn.testResult = result_
+    runner_ = runner.PluggableTestRunner(ssn) # needed??
+    ssn.testRunner = runner_
     # load and register plugins
-    # loop: read test id from conn
-    #   fire load tests by name
-    #   make recording test result
-    #   run test(s) returned by hook
-    #   send results back on conn -- list of (testid, hook, event) tuples
+    ssn.plugins = [
+        plugin(session=ssn) for plugin in session_export['pluginClasses']]
+    rlog.debug("Plugins loaded: %s", ssn.plugins)
+    for plugin in ssn.plugins:
+        plugin.register()
+        rlog.debug("Registered %s in subprocess", plugin)
+
+    event = SubprocessEvent(loader_, result_, runner_, ssn.plugins, conn)
+    res = ssn.hooks.startSubprocess(event)
+    if event.handled and not res:
+        return
+    executor = event.executeTests
+    for testid in gentests(conn):
+        if testid is None:
+            break
+        test = event.loader.loadTestsFromName(testid)
+        if event.handled:
+            continue
+        # xxx try/except?
+        rlog.debug("Execute test %s (%s)", testid, test)
+        executor(test, event.result)
+        conn.send((testid, ssn.hooks.flush()))
+        rlog.debug("Log for %s returned", testid)
+    ssn.hooks.stopSubprocess(event)
 
 
-# XXX not recording result, recording *hook*
-# class set in the session in procserver
-# case(res)
-# send back session.hooks.flush()
+# test generator
+def gentests(conn):
+    while True:
+        try:
+            testid = conn.recv()
+            if testid is None:
+                return
+            yield testid
+        except EOFError:
+            return
 
-def run_test(case):
-    res = RecordingResult()
-    case(res)
-    return res.events()
+
+# custom event classes
+class SubprocessEvent(events.Event):
+    def __init__(self, loader, result, runner, plugins, connection, **metadata):
+        self.loader = loader
+        self.result = result
+        self.runner = runner
+        self.plugins = plugins
+        self.connection = connection
+        self.excecuteTests = lambda test, result: test(result)
+        super(SubprocessEvent, self).__init__(**metadata)
 
 
-class RecordingResult(object):
+class RegisterInSubprocessEvent(events.Event):
+    def __init__(self, **metadata):
+        self.pluginClasses = []
+        super(RegisterInSubprocessEvent, self).__init__(**metadata)
+
+
+# custom hook system that records calls and events
+class RecordingHook(events.Hook):
+    def __init__(self, method, interface):
+        super(RecordingHook, self).__init__(method)
+        self.interface = interface
+
+    def __call__(self, event):
+        res = super(RecordingHook, self).__call__(event)
+        self.interface.log(self.method, event)
+        return res
+
+
+class RecordingPluginInterface(events.PluginInterface):
+    hookClass = RecordingHook
+
     def __init__(self):
-        self._events = []
+        super(RecordingPluginInterface, self).__init__()
+        self.events = []
 
-    def events(self):
-        return self._events
+    def log(self, method, event):
+        self.events.append((method, event))
 
-    def startTest(self, test):
-        pass
+    def flush(self):
+        events = self.events[:]
+        self.events = []
+        # XXX make pickle safe
+        return self._fix(events)
 
-    def stopTest(self, test):
-        pass
+    def _fix(events):
+        for event in events:
+            if hasattr(event, 'case'):
+                event.case = event.case.id()
+            if hasattr(event, 'exc_info'):
+                ec, ev, tb = event.exc_info
+                event.exc_info = (ex, ev, util.format_traceback(tb))
+            if hasattr(event, 'result'):
+                event.result = None
+            if hasattr(event, 'loader'):
+                event.loader = None
+            if hasattr(event, 'runner'):
+                event.runner = None
+            yield event
 
-    def addError(self, test, err):
-        pass
-
-    def addFailure(self, test, err):
-        pass
-
-    def addSuccess(self, test):
-        pass
-
-    def addSkip(self, test, reason):
-        pass
-
-    def addExpectedFailure(self, test, err):
-        pass
-
-    def addUnexpectedSuccess(self, test):
-        pass
+    def __getattr__(self, attr):
+        return self.hooks.setdefault(attr, self.hookClass(attr, self))

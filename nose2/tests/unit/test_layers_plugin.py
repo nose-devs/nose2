56faf4317c784e7cbd85696e1e363b28ae966393
JP <jpellerin@leapfrogonline.com>
2012-11-16 13:54:15 -0500

    Make layer unit tests order-aware

--- nose2/tests/unit/test_layers_plugin.py
+++ nose2/tests/unit/test_layers_plugin.py
@@ -30,9 +30,8 @@ class TestLayers(TestCase):
         suite = unittest.TestSuite([T2('test'), T1('test')])
         event = events.StartTestRunEvent(None, suite, None, 0, None)
         self.plugin.startTestRun(event)
-        expect = self._listset(
-            [['test (nose2.tests.unit.test_layers_plugin.T1)',
-              ['test (nose2.tests.unit.test_layers_plugin.T2)']]])
+        expect = [['test (nose2.tests.unit.test_layers_plugin.T1)',
+                   ['test (nose2.tests.unit.test_layers_plugin.T2)']]]
         self.assertEqual(self.names(event.suite), expect)
 
     def test_multiple_inheritance(self):
@@ -58,20 +57,19 @@ class TestLayers(TestCase):
         suite = unittest.TestSuite([T2('test'), T1('test'), T3('test')])
         event = events.StartTestRunEvent(None, suite, None, 0, None)
         self.plugin.startTestRun(event)
-        expect = self._listset(
-            [['test (nose2.tests.unit.test_layers_plugin.T1)',
-              ['test (nose2.tests.unit.test_layers_plugin.T3)'],
-              ['test (nose2.tests.unit.test_layers_plugin.T2)']]])
+        expect = [['test (nose2.tests.unit.test_layers_plugin.T1)',
+                   ['test (nose2.tests.unit.test_layers_plugin.T2)'],
+                   ['test (nose2.tests.unit.test_layers_plugin.T3)']]]
         self.assertEqual(self.names(event.suite), expect)
 
     def test_deep_inheritance(self):
         class L1(object):
             pass
-        class L2(object):
+        class L2(L1):
             pass
         class L3(L1):
             pass
-        class L4(L1, L2):
+        class L4(L2, L1):
             pass
         class L5(L4):
             pass
@@ -100,12 +98,11 @@ class TestLayers(TestCase):
                                     T4('test'), T5('test')])
         event = events.StartTestRunEvent(None, suite, None, 0, None)
         self.plugin.startTestRun(event)
-        expect = self._listset(
-            [['test (nose2.tests.unit.test_layers_plugin.T2)',
-              ['test (nose2.tests.unit.test_layers_plugin.T1)',
-               ['test (nose2.tests.unit.test_layers_plugin.T3)'],
-               ['test (nose2.tests.unit.test_layers_plugin.T4)',
-                ['test (nose2.tests.unit.test_layers_plugin.T5)']]]]])
+        expect = [['test (nose2.tests.unit.test_layers_plugin.T1)',
+                   ['test (nose2.tests.unit.test_layers_plugin.T2)',
+                    ['test (nose2.tests.unit.test_layers_plugin.T4)',
+                     ['test (nose2.tests.unit.test_layers_plugin.T5)']]],
+                   ['test (nose2.tests.unit.test_layers_plugin.T3)']]]
         self.assertEqual(self.names(event.suite), expect)
 
     def test_mixed_layers_no_layers(self):
@@ -129,10 +126,9 @@ class TestLayers(TestCase):
         suite = unittest.TestSuite([T2('test'), T1('test'), T3('test')])
         event = events.StartTestRunEvent(None, suite, None, 0, None)
         self.plugin.startTestRun(event)
-        expect = self._listset(
-            ['test (nose2.tests.unit.test_layers_plugin.T3)',
-             ['test (nose2.tests.unit.test_layers_plugin.T1)',
-              ['test (nose2.tests.unit.test_layers_plugin.T2)']]])
+        expect = ['test (nose2.tests.unit.test_layers_plugin.T3)',
+                  ['test (nose2.tests.unit.test_layers_plugin.T1)',
+                   ['test (nose2.tests.unit.test_layers_plugin.T2)']]]
         self.assertEqual(self.names(event.suite), expect)
 
     def test_ordered_layers(self):
@@ -175,16 +171,17 @@ class TestLayers(TestCase):
                     ['test (nose2.tests.unit.test_layers_plugin.T5)',]],
                    ['test (nose2.tests.unit.test_layers_plugin.T3)',],
                    ['test (nose2.tests.unit.test_layers_plugin.T4)',]]]
-        self.assertEqual([n for n in self.iternames(event.suite)], expect)
+        self.assertEqual(self.names(event.suite), expect)
 
     def names(self, suite):
-        n = set([])
-        for t in suite:
-            if isinstance(t, unittest.TestCase):
-                n.add(str(t))
-            else:
-                n.add(self.names(t))
-        return frozenset(n)
+        return [n for n in self.iternames(suite)]
+        # n = set([])
+        # for t in suite:
+        #     if isinstance(t, unittest.TestCase):
+        #         n.add(str(t))
+        #     else:
+        #         n.add(self.names(t))
+        # return frozenset(n)
 
     def iternames(self, suite):
         for t in suite:

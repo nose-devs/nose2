43c2ff1bbdf10f078b4ce2e6860bd54afbac6b4d
Philip Thiem <ptthiem@gmail.com>
2013-01-16 17:45:53 -0600

    Moved XML cleanup functions to util.py.  This seems like a better idea.

--- nose2/plugins/junitxml.py
+++ nose2/plugins/junitxml.py
@@ -11,53 +11,12 @@ section in a config file.
 """
 # Based on unittest2/plugins/junitxml.py,
 # which is itself based on the junitxml plugin from py.test
-import time, re, sys, six
+import time, re, sys
 from xml.etree import ElementTree as ET
 
 from nose2 import events, result, util
-from six import u
-
-def _unichr(string):
-    if six.PY3:
-        return chr(string)
-    else:
-        return unichr(string)
 
 __unittest = True
-
-_illegal_xml_rngs = [ (0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F), (0x7F, 0x84), 
-                    (0x86, 0x9F), (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), 
-                    (0xFFFE, 0xFFFF), (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), 
-                    (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF),
-                    (0x6FFFE, 0x6FFFF), (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
-                    (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF),
-                    (0xCFFFE, 0xCFFFF), (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
-                    (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF) ]
-
-_illegal_xml_rngs = [ (l, min(h, sys.maxunicode)) for (l,h) \
-    	                                  in _illegal_xml_rngs \
-                                                  if l < sys.maxunicode ]
-_illegal_xml_restr = u('[') + \
-                     u('').join(["%s-%s" % (_unichr(l), _unichr(h)) 
-                               for (l, h) in _illegal_xml_rngs]) + \
-                     u(']')
-
-_illegal_xml_re = re.compile(_illegal_xml_restr)
-
-def _match_repr(match):
-    """gets the string reprentation and strips off u'' if needed""" 
-    value = repr(match.group())
-    if value[0:2] == "u'" and value[-1:] == "'":
-        value = value[2:-1]
-    elif value[0] == "'" and value[-1:] == "'":
-        value = value[1:-1]
-
-    return value
-
-def xml_string_cleanup(string):
-    if not six.PY3:
-        string = unicode(string, errors='replace')
-    return _illegal_xml_re.sub(_match_repr, string)
 
 class JUnitXmlReporter(events.Plugin):
     """Output junit-xml test report to file"""
@@ -98,7 +57,7 @@ class JUnitXmlReporter(events.Plugin):
         elif event.reason:
             msg = event.reason
 
-        msg = xml_string_cleanup(msg)
+        msg = util.xml_string_cleanup(msg)
 
 	
 

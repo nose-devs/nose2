d328276cac4ea8bf9349441631f49352b97102e6
Tim Sampson <littlesanga@gmail.com>
2013-05-03 13:15:07 +0000

    add a help statement for 'verbose'

--- nose2/main.py
+++ nose2/main.py
@@ -163,7 +163,7 @@ class PluggableTestProgram(unittest.TestProgram):
             '--exclude-plugin', action='append',
             dest='exclude_plugins', default=[],
             help="Do not load this plugin module")
-        self.argparse.add_argument('--verbose', '-v', action='count', default=0)
+        self.argparse.add_argument('--verbose', '-v', action='count', default=0, help="print more information about tests")
         self.argparse.add_argument('--version', action='count', default=0, help="print version and exit")
         self.argparse.add_argument('--quiet', action='store_const',
                                  dest='verbose', const=0)

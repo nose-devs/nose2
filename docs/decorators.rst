7b507630cfe8f0a0378a260c5f3a52eedf0d7918
Omer Katz <omer.drow@gmail.com>
2013-06-08 20:13:24 +0300

    Added documentation for the with_setup decorator.

---
+++ docs/decorators.rst
@@ -0,0 +1,10 @@
+==========
+Decorators
+==========
+
+nose2 ships with various decorators that assist you to write your tests.
+
+Setup & Teardown
+================
+
+.. autofunction :: nose2.tools.decorators.with_setup
--- nose2/tools/decorators.py
+++ nose2/tools/decorators.py
@@ -1,4 +1,18 @@
+"""
+This module provides decorators that assist the test author to write tests.
+"""
+
+
 def with_setup(setup):
+    """
+    A decorator that sets the setup method to be executed before the test.
+
+    It currently works only for function test cases.
+
+    :param setup: The method to be executed before the test.
+    :type setup: function
+    """
+
     def decorator(testcase):
         testcase.setup = setup
 

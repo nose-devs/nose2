88696552f946fe163f6ac8288c57d6a9140ac138
Omer Katz <omer.drow@gmail.com>
2013-07-21 22:00:46 +0300

    Added the with_teardown decorator.

--- nose2/tests/functional/support/scenario/decorators/test_decorators.py
+++ nose2/tests/functional/support/scenario/decorators/test_decorators.py
@@ -1,6 +1,7 @@
-from nose2.tools.decorators import with_setup
+from nose2.tools.decorators import with_setup, with_teardown
 
 setup_performed = False
+teardown_performed = False
 
 
 def setup():
@@ -8,6 +9,20 @@ def setup():
     setup_performed = True
 
 
+def teardown():
+    global teardown_performed
+    teardown_performed = True
+
+
 @with_setup(setup)
 def test_with_setup():
     assert setup_performed, 'Setup not performed.'
+    
+
+@with_teardown(teardown)
+def test_with_teardown():
+    pass
+
+
+def test_teardown_ran():
+    assert teardown_performed, 'Teardown not performed.'

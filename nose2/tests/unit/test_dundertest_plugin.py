3ced9859b74ab579e674dfbd8772ad3ad7e31c6b
jfoo <jcjf1983@gmail.com>
2014-01-28 23:17:20 +0800

    Construct the TestCase object properly

--- nose2/tests/unit/test_dundertest_plugin.py
+++ nose2/tests/unit/test_dundertest_plugin.py
@@ -9,19 +9,23 @@ class TestDunderTestPlugin(TestCase):
     tags = ['unit']
 
     def setUp(self):
+        class DummyCase(TestCase):
+            def test_a(self):
+                pass
+
         self.suite = unittest.TestSuite()
-        self.caseClass = TestCase
+        self.caseClass = DummyCase
         self.session = session.Session()
         self.plugin = dundertest.DunderTestFilter(session=self.session)
         self.plugin.register()
 
     def test_undefined_dunder_test_attribute_keeps_test(self):
-        self.suite.addTest(self.caseClass())
+        self.suite.addTest(self.caseClass('test_a'))
         self.plugin.removeNonTests(self.suite)
         self.assertEquals(len(list(self.suite)), 1)
 
     def test_false_dunder_test_attribute_removes_test(self):
-        dummyTest = self.caseClass()
+        dummyTest = self.caseClass('test_a')
         dummyTest.__test__ = False
         self.suite.addTest(dummyTest)
         self.plugin.removeNonTests(self.suite)

98f631a2a320237b6122e308ebc454dab58cee8f
Alexander Bayandin <a.bayandin@gmail.com>
2014-04-23 14:15:15 +0400

    Add decorator cartesian_params (resolve #162)

--- nose2/tests/unit/test_params_plugin.py
+++ nose2/tests/unit/test_params_plugin.py
@@ -1,7 +1,7 @@
 from nose2 import events, loader, session, util
 from nose2.plugins.loader import parameters, testcases
 from nose2.tests._common import TestCase
-from nose2.tools import params
+from nose2.tools import cartesian_params, params
 
 
 class TestParams(TestCase):
@@ -54,18 +54,35 @@ class TestParams(TestCase):
 
         class Test(TestCase):
 
+            @cartesian_params(
+                (1, 2),
+                (2, 3),
+            )
+            def test_cartesian_params(self, first, second):
+                assert first == second
+
             @params(1, 2)
-            def test(self, a):
+            def test_params(self, a):
                 assert a == 1
+
         m = Mod()
         m.Test = Test
         Test.__module__ = m.__name__
         event = events.LoadFromModuleEvent(self.loader, m)
         self.session.hooks.loadTestsFromModule(event)
         self.assertEqual(len(event.extraTests), 1)
-        self.assertEqual(len(event.extraTests[0]._tests), 2)
-        # check that test names are sensible
+        self.assertEqual(len(event.extraTests[0]._tests), 2 + 4)
+        # check @cartesian_params for that test names are sensible
         self.assertEqual(util.test_name(event.extraTests[0]._tests[0]),
-                         'themod.Test.test:1')
+                         'themod.Test.test_cartesian_params:1')
         self.assertEqual(util.test_name(event.extraTests[0]._tests[1]),
-                         'themod.Test.test:2')
+                         'themod.Test.test_cartesian_params:2')
+        self.assertEqual(util.test_name(event.extraTests[0]._tests[2]),
+                         'themod.Test.test_cartesian_params:3')
+        self.assertEqual(util.test_name(event.extraTests[0]._tests[3]),
+                         'themod.Test.test_cartesian_params:4')
+        # check @params for that test names are sensible
+        self.assertEqual(util.test_name(event.extraTests[0]._tests[4]),
+                         'themod.Test.test_params:1')
+        self.assertEqual(util.test_name(event.extraTests[0]._tests[5]),
+                         'themod.Test.test_params:2')

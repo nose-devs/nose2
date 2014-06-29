daf4288518eb451829ae51da5d00e13651282552
Alexander Bayandin <a.bayandin@gmail.com>
2014-06-29 20:19:47 +0400

    Tests for python 3.4

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Operating System :: OS Independent',

1b2cd89bfd5272649f9795509537de6349555417
Corentin Henry <corentinhenry@gmail.com>
2016-02-13 19:04:25 +0100

    now fail

--- nose2/tests/functional/support/such/test_such_timing.py
+++ nose2/tests/functional/support/such/test_such_timing.py
@@ -1,6 +1,7 @@
 # Issue: https://github.com/nose-devs/nose2/issues/215
 # From: https://gist.github.com/lukeorland/d4534a20241d7b5280b0
 # Author: https://github.com/lukeorland
+import unittest
 import time
 from nose2.tools import such
 
@@ -10,11 +11,21 @@ def slow_blocking_init():
     return True
 
 
+class Layer1(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        it.obj = False
+
+
+class Layer2(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        it.obj = slow_blocking_init()
+
+
 with such.A('system with a fast initial setup layer') as it:
 
-    @it.has_setup
-    def setup():
-        it.obj = False
+    it.uses(Layer1)
 
     @it.should('not have obj initialized')
     def test():
@@ -22,12 +33,11 @@ with such.A('system with a fast initial setup layer') as it:
 
     with it.having('a second slow setup layer'):
 
-        @it.has_setup
-        def setup():
-            it.obj = slow_blocking_init()
+        it.uses(Layer2)
 
         @it.should('have obj initialized')
         def test():
             assert it.obj
+
 
 it.createTests(globals())

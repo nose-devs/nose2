c11ed5b047fb6a091b817727c84a8ead0ec20029
Corentin Henry <corentinhenry@gmail.com>
2016-03-01 22:13:32 +0100

    add python 3.5 support

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Operating System :: OS Independent',

57e4ef36c100956f8bb0d6e0dfe8f01cad24ded8
Stephen Rosen <sirosen@uchicago.edu>
2017-10-24 13:31:47 -0400

    Add new testcase for coverage
    Add tests that show that coveragerc show_missing and passing
    term-missing via nose2.cfg both have the same effect. Adds a pair of
    (similar) scenarios which let us test this, and refactor the
    coverage testcase a bit to make this testing easier.

---
+++ nose2/tests/functional/support/scenario/test_coverage_config/nose2cfg/covered_lib_nose2cfg/mod1.py
@@ -0,0 +1,10 @@
+def covered_func():
+    a = 1
+    a = a + 8
+    return a
+
+
+def uncovered_func():
+    b = 1
+    b = b + 8
+    return b
--- nose2/tests/functional/test_coverage.py
+++ nose2/tests/functional/test_coverage.py
@@ -4,6 +4,31 @@ from nose2.tests._common import FunctionalTestCase
 
 
 class TestCoverage(FunctionalTestCase):
+    def assertProcOutputPattern(self, proc, libname, stats,
+                                total_stats=None):
+        """
+        - proc: a popen proc to check output on
+        - libname: the name of the covered library, to build the output pattern
+        - stats: the output format for stats info
+        - total_stats: optional, distinct stats format for the TOTAL line
+                       defaults to be the same as stats
+        """
+        if total_stats is None:
+            total_stats = stats
+
+        expected = os.path.join(libname, 'mod1(.py)?')
+        expected = expected.replace('\\', r'\\')
+        expected = expected + stats
+
+        stdout, stderr = proc.communicate()
+
+        self.assertTestRunOutputMatches(
+            proc,
+            stderr=expected)
+        self.assertTestRunOutputMatches(
+            proc,
+            stderr='TOTAL\s+' + total_stats)
+
     def test_run(self):
         proc = self.runIn(
             'scenario/test_with_module',
@@ -11,14 +36,26 @@ class TestCoverage(FunctionalTestCase):
             '--with-coverage',
             '--coverage=lib/'
         )
-        STATS = '\s+8\s+5\s+38%'
-        expected = os.path.join('lib', 'mod1(.py)?')
-        expected = expected.replace('\\', r'\\')
-        expected = expected + STATS
-        stdout, stderr = proc.communicate()
-        self.assertTestRunOutputMatches(
-            proc,
-            stderr=expected)
-        self.assertTestRunOutputMatches(
-            proc,
-            stderr='TOTAL\s+' + STATS)
+        self.assertProcOutputPattern(proc, 'lib', '\s+8\s+5\s+38%')
+
+    def test_run_coverage_configs(self):
+        STATS = '\s+8\s+5\s+38%\s+1, 7-10'
+        TOTAL_STATS = '\s+8\s+5\s+38%\s'
+
+        proc = self.runIn(
+            'scenario/test_coverage_config/coveragerc',
+            '-v',
+            '--with-coverage',
+            '--coverage=covered_lib_coveragerc/'
+        )
+        self.assertProcOutputPattern(proc, 'covered_lib_coveragerc', STATS,
+                                     total_stats=TOTAL_STATS)
+
+        proc = self.runIn(
+            'scenario/test_coverage_config/nose2cfg',
+            '-v',
+            '--with-coverage',
+            '--coverage=covered_lib_nose2cfg/'
+        )
+        self.assertProcOutputPattern(proc, 'covered_lib_nose2cfg', STATS,
+                                     total_stats=TOTAL_STATS)

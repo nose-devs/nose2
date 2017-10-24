0903a60041c9215d835f35f400e0175c43014636
Stephen Rosen <sirosen@uchicago.edu>
2017-10-24 12:35:35 -0400

    Comment on versioning method in nose2._version
    This is a nonsense commit to try to debug Coveralls issues.

--- nose2/_version.py
+++ nose2/_version.py
@@ -2,4 +2,6 @@
 
 # taken from http://stackoverflow.com/a/17626524/1836144
 # The following line *must* be the last in the module, exactly as formatted:
+# could also use advice from
+# https://packaging.python.org/guides/single-sourcing-package-version/
 __version__ = '0.7.0'

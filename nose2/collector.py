eda3c99f5db1715598bcdb3e68af97cabe2a0076
Stephen Rosen <sirosen@uchicago.edu>
2017-11-09 14:41:31 -0500

    Update setuptools invocation to handle coverage
    Setuptools invocation runs differently from command-line usage. This is,
    itself, a potential source of grief.
    Try to make it behave more like `nose2` invocation by doing the
    following:
    - add ~/.{unittest,nose2}.cfg to loaded config
    - trigger some events nose2 plugins want to use
    - fix nose2.plugins.coverage to handle those events without handleArgs
    
    Unfortunately, it looks like unifying these two modes of use more
    would be a significant effort, and it's not obvious that the payoff is
    worth the reward.
    
    Resolves #317

--- nose2/collector.py
+++ nose2/collector.py
@@ -1,12 +1,19 @@
+import os
 import sys
 import unittest
 
-from nose2 import loader, runner, session
+from nose2 import loader, runner, session, events
 from nose2.main import PluggableTestProgram
 __unittest = True
 
 
 def collector():
+    """
+    This is the entry point used by setuptools, as in::
+
+      python setup.py test
+
+    """
     class Test(unittest.TestCase):
 
         def run(self, result_):
@@ -23,12 +30,49 @@ def collector():
             ssn, ldr, rnr = self._get_objects()
 
             ssn.testLoader = ldr
-            ssn.loadConfigFiles('unittest.cfg', 'nose2.cfg', 'setup.cfg')
+            ssn.loadConfigFiles('unittest.cfg', 'nose2.cfg', 'setup.cfg',
+                                os.path.expanduser('~/.unittest.cfg'),
+                                os.path.expanduser('~/.nose2.cfg'))
             ssn.setStartDir()
             ssn.prepareSysPath()
             ssn.loadPlugins(PluggableTestProgram.defaultPlugins)
 
-            test = ldr.loadTestsFromNames([], None)
+            # TODO: refactor argument parsing to make it possible to feed CLI
+            # args to plugins via this path (currently done in
+            # PluggableTestProgram)
+            # in order to do this, it seems like features in
+            # PluggableTestProgram need to be factored out into some source
+            # from which both it and this dummy test case can invoke them
+            #
+            # this is the disabled feature:
+            # ssn.hooks.handleArgs(events.CommandLineArgsEvent(...))
+            #
+            # this means that there may be plugins which don't work under
+            # setuptools invocation because they expect to get handleArgs
+            # triggered (e.g. older versions of the coverage plugin)
+
+            # FIXME: this is all a great-big DRY violation when compared with
+            # PluggableTestProgram
+
+            # create the testsuite, and make sure the createTests event gets
+            # triggered, as some plugins expect it
+            # just doing `ldr.loadTestsFromNames` works, but leaves some
+            # plugins in the lurch
+            event = events.CreateTestsEvent(ldr, [], None)
+            result = ssn.hooks.createTests(event)
+            if event.handled:
+                test = event
+            else:
+                test = ldr.loadTestsFromNames([], None)
+
+            # fire the "createdTestSuite" event for plugins to handle
+            # as above, we can get away without this, but some plugins will
+            # expect it
+            event = events.CreatedTestSuiteEvent(test)
+            result = ssn.hooks.createdTestSuite(event)
+            if event.handled:
+                test = result
+
             rslt = rnr.run(test)
             return rslt.wasSuccessful()
 

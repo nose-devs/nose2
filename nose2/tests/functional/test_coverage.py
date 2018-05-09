8ac53cf74aa942c65f4c8b455382c5aaf4a1ecfc
Roberts, Benjamin <benjamin.roberts@sky.uk>
2018-05-09 19:54:05 +0100

    Write failing test for coverage failure

--- nose2/tests/functional/test_coverage.py
+++ nose2/tests/functional/test_coverage.py
@@ -98,3 +98,15 @@ class TestCoverage(FunctionalTestCase):
         self.assertProcOutputPattern(proc, 'covered_lib', STATS,
                                      total_stats=TOTAL_STATS,
                                      assert_exit_status=1)
+
+    def test_run_coverage_fail_under_race_conditions(self):
+        STATS = '\s+8\s+5\s+38%\s+1, 7-10'
+        TOTAL_STATS = '\s+8\s+5\s+38%\s'
+
+        proc = self.runIn(
+            'scenario/coverage_config_fail_under_race_conditions',
+            '-v'
+        )
+        self.assertProcOutputPattern(proc, 'part_covered_lib', STATS,
+                                     total_stats=TOTAL_STATS,
+                                     assert_exit_status=1)

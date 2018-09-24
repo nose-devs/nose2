cf8841735fbd71965ad3a21b5c9a3cb10690a4c6
cclauss <cclauss@bluewin.ch>
2018-09-24 22:46:36 +0200

    setup.py: Add Python 3.7 trove classifier

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Operating System :: OS Independent',

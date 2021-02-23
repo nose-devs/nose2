88727e2ece502f448eaa65bbedd271ff9321c52a
rim <bendhaou.rim@gmail.com>
2021-02-22 19:39:45 -0500

    my first commit message

---
+++ nose2/plugins/plugintest.py
@@ -0,0 +1,16 @@
+"""
+test
+"""
+import logging
+
+from nose2.events import Plugin
+
+log = logging.getLogger(__name__)
+
+
+class PluginEssai(Plugin):
+    configSection = 'essai'
+    commandLineSwitch = ('e', 'with-essai', 'Essai de plugin')
+
+    def __init__(self):
+        pass

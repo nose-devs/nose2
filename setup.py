0c8b7e2e6fd152f197bf2d73bd106c4300ebba7d
Stephen Rosen <sirosen@globus.org>
2022-07-16 23:13:24 +0000

    Remove py2 from classifiers

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
